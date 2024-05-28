f3a6ef46971f66affa2f190ea4958c3c60c82e41
eun725 <127072393+kimeunkyoungg@users.noreply.github.com>
2024-05-28 16:38:36 +0900

    음성인식 오류 해결

--- src/screens/PurchaseScreen.js
+++ src/screens/PurchaseScreen.js
@@ -71,6 +71,7 @@ const PurchaseScreen = () => {
             console.log('해당하는 메뉴 이름의 빵이 없습니다.');
           }
         }
+        setResult('');
       }
       
   }, [result]); // 의존성 배열에 result를 추가
